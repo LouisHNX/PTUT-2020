2ff8431cb9abd55851bceeef06e122618788d09c
Louis <louis.garrido42@gmail.com>
2021-01-05 12:15:31 +0100

    Suppresion TestApp
    Ajout App TournoiSport

--- PTUT_2020/PTUT_2020/urls.py
+++ PTUT_2020/PTUT_2020/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from testapp.views import index
+from TournoiSport.views import index
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('testapp/',index)
+    path('TournoiSport/',index)
 ]
