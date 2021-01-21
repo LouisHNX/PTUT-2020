dbb9409a70bfddd293133b97e190f679509cef9f
Louis <louis.garrido42@gmail.com>
2021-01-21 15:01:15 +0100

    Correction urls/liens

--- Projet/TournoiSport/urls.py
+++ Projet/TournoiSport/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('gestionTournoi/', views.gestionTournoi),
-    path('createTournoi/', views.pagecreatetournoi),
+    path('gestionTournoi/', views.gestionTournoi, name="gestionTournoi"),
+    path('createTournoi/', views.pagecreatetournoi,name="creationTournoi"),
     path('listeTournois/', views.listetournois, name="listeTournois"),
     path('gestionTournoi/createPool/', views.createPool),
     path('gestionTournoi/<str:name>/', views.listteam),
